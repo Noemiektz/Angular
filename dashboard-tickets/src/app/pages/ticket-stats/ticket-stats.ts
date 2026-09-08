61f6e1b19a7e1b574c888051b904fead3512730e
Noemiektz <noemiektourza@gmail.com>
2026-09-08 10:22:11 +0200

    Init

---
+++ dashboard-tickets/src/app/pages/ticket-stats/ticket-stats.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-stats',
+  imports: [],
+  templateUrl: './ticket-stats.html',
+  styleUrl: './ticket-stats.css',
+})
+export class TicketStats {
+
+}
